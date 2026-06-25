c2a9352e023e478d07df2f1ded85b3ec73c79ddb
TTTatere <isuke116@gmail.com>
2026-06-25 12:01:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
+```javascript
+// フィルタ機能
+
 function filterGallery(category){
 
-```
-const cards = document.querySelectorAll('.card');
+    const cards = document.querySelectorAll('.card');
 
-cards.forEach(card => {
+    cards.forEach(card => {
 
-    if(category === 'all'){
-        card.classList.remove('hide');
-    }else{
-
-        if(card.classList.contains(category)){
+        if(category === 'all'){
             card.classList.remove('hide');
-        }else{
-            card.classList.add('hide');
+        } else {
+
+            if(card.classList.contains(category)){
+                card.classList.remove('hide');
+            } else {
+                card.classList.add('hide');
+            }
+
         }
 
-    }
-
-});
-```
+    });
 
 }
+
+
+// モーダル機能
 
 const modal = document.getElementById('modal');
 const modalImg = document.getElementById('modalImg');
 
 document.querySelectorAll('.card img').forEach(img => {
 
-```
-img.addEventListener('click', () => {
+    img.addEventListener('click', () => {
 
-    modalImg.src = img.src;
-    modal.classList.add('show');
+        modalImg.src = img.src;
+        modalImg.alt = img.alt;
+
+        modal.classList.add('show');
+
+    });
 
 });
-```
 
-});
+
+// モーダル外クリックで閉じる
 
 modal.addEventListener('click', () => {
 
-```
-modal.classList.remove('show');
-```
+    modal.classList.remove('show');
 
 });
+```
